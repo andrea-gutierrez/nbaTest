03cb229d78aad93b35ee806613b2ff4ea3f00802
maribel <maribell426@hotmail.com>
2021-07-12 10:29:29 -0500

    prueba 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { PagesRoutingModule } from './pages/pages.routing';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/principal/jugadorescoincidentes', pathMatch: 'full' },
-  { path: '**', redirectTo: '/principal/jugadorescoincidentes' }
+  { path: 'jugadorescoincidentes', redirectTo: 'principal/jugadorescoincidentes', pathMatch: 'full' },
+  { path: '**', redirectTo: 'principal/jugadorescoincidentes' }
 ];
 
 @NgModule({
