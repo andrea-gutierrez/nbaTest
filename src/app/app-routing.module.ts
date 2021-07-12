5dfe3c5cfd97012c64ecfb843a9265d8a740d341
maribel <maribell426@hotmail.com>
2021-07-12 09:32:18 -0500

    correcion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PagesRoutingModule } from './pages/pages.routing';
 
 const routes: Routes = [
-  { path: 'jugadorescoincidentes', redirectTo: '/principal/jugadorescoincidentes', pathMatch: 'full' },
+  { path: 'jugadorescoincidentes', redirectTo: 'principal/jugadorescoincidentes', pathMatch: 'full' },
   // { path: '', redirectTo: 'principal/listajugadores', pathMatch: 'full' },
   { path: '**', redirectTo: 'principal/jugadorescoincidentes' }
 ];
