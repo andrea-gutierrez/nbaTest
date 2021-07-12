d72d58fcfaec351fa8d16b9638eb0c909f8ef91e
maribel <maribell426@hotmail.com>
2021-07-12 09:58:15 -0500

    deployment 4

--- src/app/pages/jugadores/jugadores.routing.ts
+++ src/app/pages/jugadores/jugadores.routing.ts
@@ -1,5 +1,4 @@
 import { Routes, RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
 
 import { ListaJugadoresComponent } from './lista-jugadores/lista-jugadores.component';
 import { JugadorComponent } from './jugador/jugador.component';
